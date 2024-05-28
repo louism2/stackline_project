58ee72b41109fcf8f8f80408450ea22e3ab14edc
Louis Mancini <louismancini@Louiss-MacBook-Pro.local>
2024-05-28 10:22:08 -0700

    Remove unused CRA infrastructure

---
+++ src/components/Product.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+
+const Product: React.FC = () => {
+
+    useEffect(() => {
+
+    })
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Product
