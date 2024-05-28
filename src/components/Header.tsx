3c64ac1f486a26785fc35e5af9c54711d80a9fe3
Louis Mancini <louismancini@Louiss-MacBook-Pro.local>
2024-05-28 10:09:06 -0700

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = ()=> {
+    return (
+        <div id="header-wrapper">
+            <img id="logo" src="stackline_logo.svg" />
+        </div>
+    )
+}
+
+export default Header
